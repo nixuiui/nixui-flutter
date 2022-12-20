f2e0bc54713d0ddbb66ed0ef11b5ab5466e06a4e
nixuiui <nikirahmadiwiharto@gmail.com>
2022-12-20 15:25:37 +0700

    export permission_handler

---
+++ lib/permission_handler.dart
@@ -0,0 +1,15 @@
+export 'package:permission_handler/permission_handler.dart'
+    show
+        Permission,
+        PermissionStatus,
+        PermissionStatusGetters,
+        PermissionWithService,
+        FuturePermissionStatusGetters,
+        ServiceStatus,
+        ServiceStatusGetters,
+        FutureServiceStatusGetters,
+        PermissionActions,
+        PermissionCheckShortcuts,
+        ServicePermissionActions,
+        PermissionListActions,
+        openAppSettings;
